d6ec1b5f5ac95d7ee5df2659d83f8f9b707dd305
Michael <m_reinert@outlook.com>
2023-03-10 10:58:24 +0100

    modified height from banner on desktop

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 module.exports = {
-	content: [ './index.html' ],
+	content: ['./index.html'],
 	theme: {
 		extend: {
 			colors: {
 				'ps-blue': '#004B93'
-			}
+			},
+			height: {
+				'128': '740px'
+			},
 		}
 	},
 	plugins: []
